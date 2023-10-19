12e7526e2568e7543e83c386437053c95fd77073
DongKyu Ryu <fbehdrb97@naver.com>
2023-10-19 14:03:25 +0900

    id pwd 입력후 DB 비교후 로그인 or 로그인 실패

--- JavaBook/src/java6/p401/TextFieldTest.java
+++ JavaBook/src/java6/p401/TextFieldTest.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class TextFieldTest extends WindowAdapter implements ActionListener {
 	private Frame f, mf;
-	private Button login;
+	private Button login, b1, b2, b3, b4, Register;
 	TextField id;
 	TextField pwd;
 	TextField trueandfalse;
@@ -22,22 +22,35 @@ public class TextFieldTest extends WindowAdapter implements ActionListener {
 	public TextFieldTest() {
 		dao = new MemberDAO();
 		f = new Frame("Login");
-		f.setSize(450, 130);
+		f.setSize(450, 135);
 		f.setLayout(null);
 		f.setBackground(Color.LIGHT_GRAY);
 		f.addWindowListener(this);
-		
+
 		mf = new Frame("Main");
 		mf.setSize(900, 600);
 		mf.setLayout(null);
 		mf.setBackground(Color.pink);
 		mf.addWindowListener(this);
-		
+
+		b1 = new Button("뚜");
+		b1.setBounds(375, 300, 50, 50);
+		b2 = new Button("치");
+		b2.setBounds(425, 300, 50, 50);
+		b3 = new Button("따");
+		b3.setBounds(475, 300, 50, 50);
+		b4 = new Button("쨍!!!");
+		b4.setBounds(525, 300, 50, 50);
 
 		login = new Button("Login");
 		login.setBackground(Color.CYAN);
 		login.setBounds(370, 35, 60, 50);
 		login.addActionListener(this);
+
+		Register = new Button("Register");
+		Register.setBackground(Color.CYAN);
+		Register.setBounds(370, 90, 60, 30);
+		Register.addActionListener(this);
 
 		Label lid = new Label("ID : ", Label.RIGHT);
 		lid.setBounds(60, 35, 25, 25);
@@ -55,7 +68,7 @@ public class TextFieldTest extends WindowAdapter implements ActionListener {
 		trueandfalse = new TextField();
 		trueandfalse.setBackground(Color.pink);
 		trueandfalse.setForeground(Color.blue);
-		trueandfalse.setBounds(20, 90, 410, 20);
+		trueandfalse.setBounds(20, 90, 325, 30);
 		pwd.setEchoChar('*');
 
 		f.add(lid);
@@ -64,7 +77,12 @@ public class TextFieldTest extends WindowAdapter implements ActionListener {
 		f.add(pwd);
 		f.add(trueandfalse);
 		f.add(login);
+		f.add(Register);
 		f.setVisible(true);
+		mf.add(b1);
+		mf.add(b2);
+		mf.add(b3);
+		mf.add(b4);
 		mf.setVisible(false);
 	}
 
@@ -73,17 +91,21 @@ public class TextFieldTest extends WindowAdapter implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (!id.getText().equals("") && !pwd.getText().equals("")) {
-			ArrayList<MemberVo> ar = dao.list(id.getText());
-			if (ar.size() != 0 && ar.get(0).getPwd().equals(pwd.getText())) {
-				trueandfalse.setText("로그인이 되었습니다.");
-				mf.setVisible(true);
-				f.setVisible(false);
+		if (e.getActionCommand().equals("Login")) {
+			if (!id.getText().equals("") && !pwd.getText().equals("")) {
+				ArrayList<MemberVo> ar = dao.list(id.getText());
+				if (ar.size() != 0 && ar.get(0).getPwd().equals(pwd.getText())) {
+					trueandfalse.setText("로그인이 되었습니다.");
+					mf.setVisible(true);
+					f.setVisible(false);
+				} else {
+					trueandfalse.setText("아이디 비밀번호가 틀림");
+				}
 			} else {
-				trueandfalse.setText("아이디 비밀번호가 틀림");
+				trueandfalse.setText("틀렸습니다.");
 			}
-		} else {
-			trueandfalse.setText("틀렸습니다.");
+		} else if (e.getActionCommand().equals("Register")) {
+			trueandfalse.setText("지금은 회원가입을 할수 업습니다.");
 		}
 	}
 
